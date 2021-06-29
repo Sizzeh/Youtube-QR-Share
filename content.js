88a3554e4c2e9980fa469f24de5e08a25ac01ff2
Sizzeh <josdou00357@gmail.com>
2021-06-29 15:37:45 -0300

    update

--- content.js
+++ content.js
@@ -1,40 +1,30 @@
-var qrCount = 0;
-var qrCode
-var firstCode = true
-var lastUrl;
-
+var qrCount = 0,
+    qrCode,
+    firstCode = true,
+    lastUrl,
+    url;
 function generateQR() {
-    if (document.getElementById('share-url')) {
-        var url = document.getElementById('share-url').value
-    }
+    if (document.getElementById('share-url')) url = document.getElementById('share-url').value; // grab share url
         if (url && document.getElementsByClassName('yt-copy-link-renderer')[0]) {
-            if (firstCode == true) {
+            if (firstCode == true) { // generate qr code placeholder
                 qrCode = new QRCode(document.getElementById('bar'), {
                     width: 100,
                     height: 100
-                })
-                qrCode.makeCode(url)
+                });
+                qrCode.makeCode(url);
                 firstCode = false;
             }
             else {
-                if (lastUrl != url) {
+                if (lastUrl != url) { // only update if the share url changes
                     lastUrl = url;
                     console.log(`[Youtube QR Share] Updating QR CODE`);
-                    qrCode.makeCode(url)
+                    qrCode.makeCode(url);
                 }
             }
         }
 }
-
 setTimeout(() => {
-    generateQR()
-    //console.log(document.getElementsByClassName("ytd-button-renderer"))
-    //document.getElementsByClassName("ytd-button-renderer")
-    let btnList = document.getElementsByClassName("ytd-button-renderer")
-    for (let i = 0; i < btnList.length; i++) {
-        //btnList[i].onclick = function() { generateQR(); }
-    }
     setInterval(() => {
-        generateQR()
+        generateQR();
     }, 1000);
 }, 1500);
